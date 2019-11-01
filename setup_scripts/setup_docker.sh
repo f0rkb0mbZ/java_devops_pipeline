530d13e72c130a1955c459c971c8f1c40be3fb03
Snehangshu Bhattacharya <snehangshubhattacharya@yahoo.in>
2019-11-01 11:30:22 +0530

    container runtime setting changed to systemd in docker

--- setup_scripts/setup_docker.sh
+++ setup_scripts/setup_docker.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 sudo apt-get remove docker docker-engine docker.io containerd runc
+
 sudo apt-get update
+
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common
+    
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
 sudo apt-key fingerprint 0EBFCD88
+
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
 sudo apt-get update
+
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
